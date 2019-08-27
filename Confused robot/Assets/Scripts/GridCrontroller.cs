72d283193f5abe252aabb2445854d4b75e6f47a5
Bernardo <behornerl@gmail.com>
2019-08-27 01:41:07 -0300

    Script de controle da grid inicializando a grid

---
+++ Confused robot/Assets/Scripts/GridCrontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCrontroller : MonoBehaviour
+{
+    [SerializeField] private int gridSize = 7;
+    private Tile[][] grid; // 1st [] = line / 2nd [] = column
+
+    // Initializes the grid
+    void Start()
+    {
+        grid = new Tile[gridSize][];
+        int test = 0;
+        for(int i = 0 ; i < gridSize ; i++)
+        {
+            Transform currentLine = transform.GetChild(i);
+            grid[i] = new Tile[7];
+            for(int j = 0 ; j < gridSize ; j++)
+            {
+                grid[i][j] = currentLine.GetChild(j).GetComponent<Tile>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
