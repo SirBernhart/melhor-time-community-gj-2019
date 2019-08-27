76ffe3f23cc4919c54447b3e87ec5ff05cead3ba
Bernardo <behornerl@gmail.com>
2019-08-27 02:50:37 -0300

    MoveEntity method, Tile now saves it's position

--- Confused robot/Assets/Scripts/GridCrontroller.cs
+++ Confused robot/Assets/Scripts/GridCrontroller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridCrontroller : MonoBehaviour
 {
-    public enum TileState {Clear, Obstacle, Danger};
+    public enum TileState {Clear, Obstacle, Danger, Enemy, Player};
 
     [SerializeField] private int gridSize = 7;
     private Tile[][] grid; // 1st [] = line / 2nd [] = column
@@ -14,15 +14,57 @@ public class GridCrontroller : MonoBehaviour
     {
         grid = new Tile[gridSize][];
 
-        for(int i = 0 ; i < gridSize ; i++)
+        for(int i = 0 ; i < gridSize ; i++) // Lines
         {
             Transform currentLine = transform.GetChild(i);
             grid[i] = new Tile[7];
-            for(int j = 0 ; j < gridSize ; j++)
+            for(int j = 0 ; j < gridSize ; j++) // Columns
             {
                 grid[i][j] = currentLine.GetChild(j).GetComponent<Tile>();
+                grid[i][j].position = new Vector2(i, j);
             }
         }
+    }
+
+    // Moves the entity, in the passed entityPos, tilesToMoveInX and tilesToMoveInY tiles
+    // Returns the entity's new position
+    public Vector2 MoveEntity (Vector2 entityPos, int tilesToMoveInX, int tilesToMoveInY)
+    {
+        // Entity's pos in int
+        int ePosX = (int) entityPos.x, 
+            ePosY = (int)entityPos.y;
+
+        // Getting the entity in the (ePosX, ePosY) grid position
+        Tile entity = grid[ePosX][ePosY];
+
+        // If the passed entity isn't moveable, don't do anything 
+        if(entity.GetState() != TileState.Player && entity.GetState() != TileState.Enemy)
+        {
+            return entityPos;
+        }
+
+        // If the passed entity's move isn't possible, don't do anything
+        if (CheckIfCanMove(entity, tilesToMoveInX, tilesToMoveInY))
+        {
+            return entityPos;
+        }
+    }
+
+    private bool CheckIfCanMove(Tile tile, int movX, int movY)
+    {
+        int newXPos = (int) tile.position.x + movX,
+            newYPos = (int) tile.position.y + movY;
+
+        if (newXPos >= gridSize ||
+            newXPos + movX < 0 ||
+            newYPos + movY >= gridSize ||
+            newYPos < 0 ||
+            grid[newXPos][newYPos].GetState() == TileState.Obstacle)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     // Update is called once per frame
