8e09ef404c3a852778aa6d33fbd48339acf1681e
Bernardo <behornerl@gmail.com>
2019-08-28 23:11:13 -0300

    Lógica de movimento da grid básica funcional

--- Confused robot/Assets/Scripts/GridCrontroller.cs
+++ Confused robot/Assets/Scripts/GridCrontroller.cs
@@ -24,48 +24,99 @@ public class GridCrontroller : MonoBehaviour
                 grid[i][j].position = new Vector2(i, j);
             }
         }
+        Debug.Log(grid[0][0].GetState());
+        Debug.Log(CheckIfCanMove(grid[0][0].position, -1f, false));
+
     }
 
-    // Moves the entity, in the passed entityPos, tilesToMoveInX and tilesToMoveInY tiles
+    // Moves the entity found in the grid's passed entityPos position "tilesToMove" in the x (if moveInX == true), else in the y axis  
     // Returns the entity's new position
-    public Vector2 MoveEntity (Vector2 entityPos, int tilesToMoveInX, int tilesToMoveInY)
+    // ===> If the new position is negative, it means the player has died
+    public Vector2 MoveEntity (Vector2 entityPos, float tilesToMove, bool moveInX)
     {
-        // Entity's pos in int
-        int ePosX = (int) entityPos.x, 
-            ePosY = (int)entityPos.y;
+        Tile currentTile = grid[(int)entityPos.x][(int)entityPos.y];
+        
 
-        // Getting the entity in the (ePosX, ePosY) grid position
-        Tile entity = grid[ePosX][ePosY];
-
-        // If the passed entity isn't moveable, don't do anything 
-        if(entity.GetState() != TileState.Player && entity.GetState() != TileState.Enemy)
+        if(CheckIfCanMove(entityPos, tilesToMove, moveInX))
         {
-            return entityPos;
-        }
+            Tile newEntityTile = GetTileInNewPosition(entityPos, tilesToMove, moveInX);
+            if (newEntityTile == null)
+            {
+                return entityPos;
+            }
+            // Instances where the player dies
+            if (currentTile.GetState() == TileState.Enemy)
+            {
+                if(newEntityTile.GetState() == TileState.Player)
+                {
+                    return new Vector2(-1, -1);
+                }
+            }
+            else if(currentTile.GetState() == TileState.Player)
+            {
+                if(newEntityTile.GetState() == TileState.Danger || newEntityTile.GetState() == TileState.Enemy) 
+                {
+                    return new Vector2(-1, -1);
+                }
+            }
 
-        // If the passed entity's move isn't possible, don't do anything
-        if (CheckIfCanMove(entity, tilesToMoveInX, tilesToMoveInY))
-        {
-            return entityPos;
+            // If the player doesn't die with the move
+            newEntityTile.SetState(currentTile.GetState());
+            currentTile.SetState(TileState.Clear);
+
+            return newEntityTile.position;
+
         }
-        return Vector2.zero;
+        return entityPos;
     }
 
-    private bool CheckIfCanMove(Tile tile, int movX, int movY)
+    // Checks if the entity is capable of moving in the passed direction
+    private bool CheckIfCanMove(Vector2 entityPos, float tilesToMove, bool moveInX)
     {
-        int newXPos = (int) tile.position.x + movX,
-            newYPos = (int) tile.position.y + movY;
+        Tile newEntityTile = GetTileInNewPosition(entityPos, tilesToMove, moveInX);
 
-        if (newXPos >= gridSize ||
-            newXPos + movX < 0 ||
-            newYPos + movY >= gridSize ||
-            newYPos < 0 ||
-            grid[newXPos][newYPos].GetState() == TileState.Obstacle)
+        if(newEntityTile == null)
         {
+            Debug.Log("Reached end of grid!");
+            return false;
+        }
+
+        if (grid[(int)newEntityTile.position.x][(int)newEntityTile.position.y].GetState() == TileState.Obstacle)
+        {
+            Debug.Log("There is an obstacle on the way");
             return false;
         }
 
         return true;
+    }
+
+    // Gets the Tile in the position arrived when moving "tilesToMove" tiles from "currentTilePos" in the passed axis
+    private Tile GetTileInNewPosition (Vector2 currentTilePos, float tilesToMove, bool moveInX)
+    {
+        int newXPos = (int)currentTilePos.x + (int)tilesToMove,
+            newYPos = (int)currentTilePos.y + (int)tilesToMove;
+        if (moveInX)
+        {
+            newXPos = (int)currentTilePos.x + (int)tilesToMove;
+            newYPos = (int)currentTilePos.y;
+        }
+        else
+        {
+            newXPos = (int)currentTilePos.x;
+            newYPos = (int)currentTilePos.y + (int)tilesToMove;
+        }
+
+        // If the new position surpasses the grid's borders, returns null
+        if(newXPos >= gridSize ||
+            newXPos < 0 ||
+            newYPos >= gridSize ||
+            newYPos < 0)
+        {
+            return null;
+        }
+
+        // Returning the tile in the (newXPos, newYPos) grid position
+        return grid[newXPos][newYPos];
     }
 
     // Update is called once per frame
