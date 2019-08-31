3dc38b3e552ad4af369bbae6832c03ab991c0f14
Bernardo <behornerl@gmail.com>
2019-08-31 01:02:44 -0300

    Door and Button tile states added

--- Confused robot/Assets/Scripts/GridCrontroller.cs
+++ Confused robot/Assets/Scripts/GridCrontroller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridCrontroller : MonoBehaviour
 {
-    public enum TileState {Clear, Obstacle, Danger, Enemy, Player};
+    public enum TileState {Clear, Obstacle, Danger, Enemy, Player, Button, Door};
 
     [SerializeField] private int gridSize = 7;
     private Tile[][] grid; // 1st [] = line / 2nd [] = column
